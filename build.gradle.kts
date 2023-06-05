883059e2c3b892a5c4bfd6b2bf44f15540321f57
lgrignon <louis.grignon@protonmail.com>
2023-06-05 08:19:56 +0200

    fix configuration for ktor and serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,10 @@ val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
-    id("io.ktor.plugin") version "2.3.0"
+
+    // ces deux plugins Gradle sont nécessaires pour que Ktor et la sérialisation fonctionnent pleinement
+    id("io.ktor.plugin") version "2.3.1"
+    kotlin("plugin.serialization") version "1.8.21"
 }
 
 application {
@@ -19,6 +22,11 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("io.ktor:ktor-client-core:$ktor_version")
+
+    // ces dépendances permettent d'assurer la dé/sérialisation de JSON vers des objets
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
+    // fournir une implémentation de client HTTP pour Ktor
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
 }
