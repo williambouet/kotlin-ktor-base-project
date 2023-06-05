151d218ca4a256162b42561fe52ff8bb2bfc9342
lgrignon <louis.grignon@protonmail.com>
2023-06-05 09:02:23 +0200

    quest correction

--- src/main/kotlin/fr.wildcodeschool.kotlinsample/main.kt
+++ src/main/kotlin/fr.wildcodeschool.kotlinsample/main.kt
@@ -1,12 +1,53 @@
 package fr.wildcodeschool.kotlinsample
 
-import io.ktor.http.*
-import io.ktor.util.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsChannel
+import io.ktor.http.Url
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.util.cio.writeChannel
+import io.ktor.utils.io.copyAndClose
+import kotlinx.serialization.json.Json
 import kotlinx.coroutines.*
-import java.io.*
-import java.text.*
-import java.util.*
+import java.io.File
+import java.nio.file.Files
 
-fun main() {
-    println("Hello Wilders !!")
+class SpaceXApi {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                useAlternativeNames = false
+            })
+        }
+    }
+
+    suspend fun getAllLaunches(): List<RocketLaunch> {
+        return httpClient.get("https://api.spacexdata.com/v5/launches").body()
+    }
+
+    private val targetDir = Files.createTempDirectory("spacex-articles")
+    suspend fun downloadArticle(launch: RocketLaunch) {
+        launch.links.article?.let { articleUrl ->
+            val url = Url(articleUrl)
+
+            val targetPath = targetDir.resolve("article_" + launch.flightNumber)
+            println("download to " + targetPath)
+            httpClient.get(url).bodyAsChannel().copyAndClose(targetPath.toFile().writeChannel())
+        }
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    val service = SpaceXApi()
+    val launches: List<RocketLaunch> = service.getAllLaunches()
+    for (l in launches) {
+        async {
+            println("download article for launch ${l.flightNumber}")
+            service.downloadArticle(l)
+            println("DONE download article for launch ${l.flightNumber}")
+        }
+    }
 }
